9a4fb03085259206a69b95b8f3964c57c2f43f5d
paufau <pakseev.1999@gmail.com>
2026-08-07 15:27:48 +0400

    Add archiving script

---
+++ archive.sh
@@ -0,0 +1,13 @@
+cd "$(dirname "$0")"
+
+plugin_cfg="addons/itch_loading_indicator_page/plugin.cfg"
+version="$(sed -n 's/^version="\(.*\)"$/\1/p' "$plugin_cfg")"
+
+if [ -z "$version" ]; then
+	echo "No version found in $plugin_cfg" >&2
+	exit 1
+fi
+
+mkdir -p dist
+git archive --prefix=ilip/ -o "./dist/ilip_${version}.zip" HEAD
+echo "Export successful! (dist/ilip_${version}.zip)"
